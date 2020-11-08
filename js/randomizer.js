02f998e21d47cde1436cf1ee6b684396d915095d
Beezxmaster <gmal48709@gmail.com>
2020-11-08 18:59:09 +0100

    Better Documentation

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,11 +1,8 @@
 /*
-    VERSION: 1.1.0 | BY BEEZXMASTER
+    VERSION: 1.1.4 | BY BEEZXMASTER
  */
 
 function randomzizer() {
-
-    /* Know when somebody press the button*/
-    const button = document.getElementById('button').value;
 
     /* Get the Body Tag, for set the background later*/
     const body = document.querySelector('body');
@@ -16,12 +13,16 @@ function randomzizer() {
     /* Get the hex-color id from html doc to set the span */
     const color = document.querySelector('span');
 
+        /* Create Veriable for prefix it to a generated code */
         let hex = '#';
 
+        /* Randomized a code of the variable hexcodes */
         for(let i = 0; i < 6; i++) {
             const random = Math.floor(Math.random()*hexcodes.length);
             hex += hexcodes[random];
         }
+
+        /* Set the color span and the body backgroundcolor of variable hex */
         color.innerHTML = hex;
         body.style.backgroundColor = hex;
 
